3f397b74365ce3d7697fafba87cd09f94fbbc07d
kipling <kiplingmarich@gmail.com>
2022-11-21 10:55:55 +0300

    update

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+  function mpesaPayment() {
+    return "mpesaPayment";
+  };
+
+  function cardPayment() {
+    return "cardPayment";
+  };
+
+
+ 
+  function mobilePayout() {
+    return "mobilePayout";
+  };
+
+  function domesticBanksPayout() {
+  
+    return "domesticBanksPayout";
+  };
+
+  module.exports = {
+    mpesaPayment,
+    cardPayment,
+    mobilePayout,
+    domesticBanksPayout,
+
+ }
